61161c2c4ac2c5c4caeec70e457c37b6a7e219e8
Greg Bloom <gbloom3@cnm.edu>
2016-10-19 22:56:34 -0600

    Added milestone-1.php

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>PWP: Milestone 1</title>
+	</head>
+	<body>
+		<header>
+			<h1>Greg Bloom Photography - PWP: Milestone 1</h1>
+		</header>
+		<main>
+		<p><strong>Purpose:</strong> Providing a platform to display my photography.</p>
+		<p><strong>Audience:</strong> Most likely, this site will be viewed by friends or potential customers/clients</p>
+		<p><strong>Goal:</strong> There are two main goals for this PWP: To attract potential customers/clients and to provide a gallery of my work </p>
+
+		</main>
+	</body>
+</html>
