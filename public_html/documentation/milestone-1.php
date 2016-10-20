592269e18b0926dcd0d29eacf8b4cbd8c799efd5
Greg Bloom <gbloom3@cnm.edu>
2016-10-20 00:08:30 -0600

    Added persona and use case to milestone-1.php

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,13 +6,30 @@
 	</head>
 	<body>
 		<header>
-			<h1>Greg Bloom Photography - PWP: Milestone 1</h1>
+			<h1>Greg Bloom Photography - PWP Milestone 1</h1>
+			<hr>
 		</header>
 		<main>
-		<p><strong>Purpose:</strong> Providing a platform to display my photography.</p>
-		<p><strong>Audience:</strong> Most likely, this site will be viewed by friends or potential customers/clients</p>
-		<p><strong>Goal:</strong> There are two main goals for this PWP: To attract potential customers/clients and to provide a gallery of my work </p>
-
+			<p><strong>Purpose:</strong> Providing a platform to display my photography.</p>
+			<p><strong>Audience:</strong> Most likely, this site will be viewed by friends or potential customers/clients</p>
+			<p><strong>Goal:</strong> There are two main goals for this site: To attract potential customers/clients and to provide a gallery of my work </p>
+			<!-- Begin Persona Section -->
+			<section>
+				<h2>Persona</h2>
+				<p><strong>Name:</strong> David Wilson</p>
+				<p><strong>Age:</strong> 27</p>
+				<p><strong>Profession:</strong> Musician, Starbucks Barista</p>
+				<p><strong>Technology:</strong> 15" HP laptop, running Windows 10</p>
+				<p><strong>Attitudes and Behaviors:</strong> David is short on time since he is still preparing his album. As such, he will most likely only spend about 5 minutes viewing images on a site before moving on.</p>
+				<p><strong>Frustrations and Needs:</strong>David gets frustrated with sites that are difficult to navigate, so he needs a simplistic but refined layout. </p>
+				<p><strong>Goals:</strong>David is only concerned with on thing: Is this photographer the best choice to assist me on my new album?</p>
+			</section>
+			<!-- Begin Use Case Section -->
+			<section>
+				<h2>Use Case</h2>
+				<p>David Wilson has searched through trending photographers and stumbled upon my profile. Based on a brief look at my profile, he found that my style is similar to what he is looking for. He finds a link to my website and adds it to a list of ones to go through the following day.</p>
+				<p>It is 8:00AM Tuesday morning, and David has a shift in half an hour. In addition to myself, he has 3 other sites to view.</p>
+			</section>
 		</main>
 	</body>
 </html>
