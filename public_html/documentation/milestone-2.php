d23354bd2c80d4e116cadadfedfae94cb17ba20c
Greg Bloom <gbloom3@cnm.edu>
2016-10-30 21:15:37 -0600

    added milestone-2.php

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+		<title>PWP: Milestone 2</title>
+	</head>
+	<body>
+		<header>
+			<h1>Greg Bloom Photography - PWP Milestone 2</h1>
+			<hr>
+		</header>
+		<main>
+			<h2>Content Strategy</h2>
+			<p>My site will consist of one page, containing the following sections:</p>
+
+			<p><strong>Welcome:</strong> A brief introduction that states my name and role as a Photographer. This welcome message will be overlaid on my favorite photograph.</p>
+
+			<p><strong>About Me:</strong> This section will detail my previous photography projects, as well as the equipment I use on a regular basis. The tone will be casual and friendly, as I am more interested in projects that are more open-ended and allow for my own personal style.</p>
+
+			<p><strong>Gallery:</strong> I want this to be a responsive marquee-style image gallery containing examples of my best work. I want to have it display only 4-6 images at a time. Gallery pop-up will include location, a descriptive paragraph of what was happening, and what equipment was used. I want to also include links to share the image on social media.</p>
+
+			<p><strong>Contact:</strong> This section will contain a simple contact form.</p>
+
+			<p><strong>Footer:</strong> This will contain links to my professional social media profiles: Twitter, Flickr, and LinkedIn.</p>
+
+			<h2>Wireframes</h2>
+			<p>This site will feature one layout that will be fully responsive on small and large screens. These wireframes were created using <a href="https://balsamiq.com">https://balsamiq.com</a>.</p>
+			<h3 class="image-header">Mobile Layout</h3>
+			<img src="img/mobile-layout.png" alt="Layout for mobile users"/>
+			<h3 class="image-header">Full-Screen Layout</h3>
+			<img src="img/browser-layout.png" alt="Layout for full-screen users"/>
+		</main>
+	</body>
+</html>
