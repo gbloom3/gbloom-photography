24b17c6d67920321a71ed1c78d278106efff6afd
Greg Bloom <gbloom3@cnm.edu>
2016-11-20 23:06:17 -0700

    added index.php and began setting it up

---
+++ public_html/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<meta http-equiv="x-ua-compatible" content="ie-edge"/>
+		<!-- Latest compiled and minified Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified Flickity CSS-->
+		<link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+
+		<!--my css file-->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- load jQuery -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+		<!-- Latest compiled and minified Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<!-- Latest compiled and minified Flickity JavaScript -->
+		<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+
+		<!-- Link to font-awesome cdn-->
+		<script src="https://use.fontawesome.com/9ae9d99d69.js"></script>
+		<title>Greg Bloom Photography</title>
+	</head>
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<main>
+				<div class="container">
+					<div class="row" id="title">
+					<div class="col-12-xs col-4-md">
+						<div>
+							<h1>Greg Bloom</h1>
+							<p>Photographer</p>
+						</div>
+					</div>
+				</div>
+					<div class="row" id="about">
+						<div class="col-12-xs col-4-md">
+						</div>
+					</div>
+					<div class="row" id="gallery">
+						<div class="col-12-xs col-4-md">
+							<div class="carousel"
+								  data-flickity='{ "wrapAround": true }'>
+								<div class="carousel-cell"></div>
+								<div class="carousel-cell"></div>
+								<div class="carousel-cell"></div>
+								<div class="carousel-cell"></div>
+								<div class="carousel-cell"></div>
+							</div>
+						</div>
+					</div>
+					<div class="row" id="contact">
+
+					</div>
+				</div>
+			</main>
+		</div>
+		<!-- begin footer-->
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-8 col-md-4">
+						<a href="https://500px.com/gbloom2010"><i class="fa fa-500px fa-3x"></i></a>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
