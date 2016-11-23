5b6f94cfd307a04420cae00295178564f1cecd2f
Greg Bloom <gbloom3@cnm.edu>
2016-11-22 19:08:58 -0700

    added lightbox plugin and formatted page with bootstrap

--- public_html/index.php
+++ public_html/index.php
@@ -4,14 +4,14 @@
 		<meta charset="UTF-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<meta http-equiv="x-ua-compatible" content="ie-edge"/>
+
 		<!-- Latest compiled and minified Bootstrap CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-		<!-- Latest compiled and minified Flickity CSS-->
-		<link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 		<!--my css file-->
 		<link rel="stylesheet" href="css/style.css" type="text/css">
@@ -27,53 +27,192 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
 		<!-- Latest compiled and minified Bootstrap JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-		<!-- Latest compiled and minified Flickity JavaScript -->
-		<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
 
 		<!-- Link to font-awesome cdn-->
 		<script src="https://use.fontawesome.com/9ae9d99d69.js"></script>
+
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+		<link rel="stylesheet" type="text/css" href="css/lightcase.css">
+		<script type="text/javascript" src="js/lightcase.js"></script>
+
+		<script type="text/javascript">jQuery(document).ready(function($) {
+				$('a[data-rel^=lightcase]').lightcase();
+			});
+		</script>
+
 		<title>Greg Bloom Photography</title>
 	</head>
 	<body class="sfooter">
 		<div class="sfooter-content">
 			<main>
-				<div class="container">
+				<div class="fluid-container">
 					<div class="row" id="title">
-					<div class="col-12-xs col-4-md">
-						<div>
-							<h1>Greg Bloom</h1>
-							<p>Photographer</p>
-						</div>
-					</div>
-				</div>
-					<div class="row" id="about">
-						<div class="col-12-xs col-4-md">
-						</div>
-					</div>
-					<div class="row" id="gallery">
-						<div class="col-12-xs col-4-md">
-							<div class="carousel"
-								  data-flickity='{ "wrapAround": true }'>
-								<div class="carousel-cell"></div>
-								<div class="carousel-cell"></div>
-								<div class="carousel-cell"></div>
-								<div class="carousel-cell"></div>
-								<div class="carousel-cell"></div>
+						<div class="col-xs-10 col-md-10 col-xs-offset-1 col-md-offset-1 backgroundImage">
+							<div class="col-xs-12 col-md-3 col-md-offset-7">
+								<div class="text-center">
+									<h1>Greg Bloom</h1>
+									<p>Photographer</p>
+								</div>
 							</div>
 						</div>
 					</div>
-					<div class="row" id="contact">
+					<div class="row" id="about">
+						<div class="col-xs-10 col-md-10 col-xs-offset-1 col-md-offset-1">
+							<p>Every day is taco ipsum tuesday. Tacos for breakfast, lunch and dinner. Um, Tabasco? No thanks,
+								do you have any Cholula? Josh’s taco shack is the best taco shack. I think I’ve overdosed on
+								tacos. Side of rice and beans, please. Side of rice and beans, please. Carne asada on corn
+								tortillas. Let’s do a beef and a chicken, and one with both. Carne asada on corn tortillas.
+								Tacos for breakfast, lunch and dinner. You see, the refried beans are really just the adhesive
+								necessary to apply the soft tortilla to the hard taco shell.</p>
 
+							<p>Flour or corn tortillas? Um, Tabasco? No thanks, do you have any Cholula? Does guac cost extra?
+								Let’s do a beef and a chicken, and one with both. It’s taco Tuesday Monday. You see, the refried
+								beans are really just the adhesive necessary to apply the soft tortilla to the hard taco shell.
+								Tacos, tacos, tacos. Ooh, with diced onions and a pinch of cilantro. How do you feel about hard
+								shelled tacos? It’s taco time all the time. Side of rice and beans, please.</p>
+						</div>
+					</div>
+					<div class="row" id="gallery">
+						<div class="col-xs-12 col-md-12 col-xs-offset-1 col-md-offset-1">
+						<h2>Gallery</h2>
+						</div>
+						<div class="col-xs-10 col-md-10 col-xs-offset-1 col-md-offset-1 text-center">
+							<script type="text/javascript" src="js/jquery.events.touch.js">
+								$('a[data-rel^=lightcase]').lightcase({
+									swipe: true
+								});
+							</script>
+							<div class="row">
+								<div class="col-xs-3 col-md-3">
+									<a href="img/photos/bee-min.jpg" class="thumbnail"
+										data-rel="lightcase:myCollection:slideshow" title="Bee">
+										<img src="img/photos/thumbs/bee-tn.jpg"/>
+									</a>
+								</div>
+								<div class="col-xs-3 col-md-3">
+									<a href="img/photos/bumblebee-min.jpg" class="thumbnail"
+										data-rel="lightcase:myCollection:slideshow"
+										title="Your title">
+										<img src="img/photos/thumbs/bumblebee-tn.jpg"/>
+									</a>
+								</div>
+								<div class="col-xs-3 col-md-3">
+									<a href="img/photos/burning-sky-min.jpg" class="thumbnail"
+										data-rel="lightcase:myCollection:slideshow"
+										title="Your title">
+										<img src="img/photos/thumbs/burning-sky-tn.jpg"/>
+									</a>
+								</div>
+								<div class="col-xs-3 col-md-3">
+									<a href="img/photos/camel-rock-min.jpg" class="thumbnail"
+										data-rel="lightcase:myCollection:slideshow"
+										title="Your title">
+										<img src="img/photos/thumbs/camel-rock-tn.jpg"/>
+									</a>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-xs-3 col-md-3">
+									<a href="img/photos/dont-look-at-me-min.jpg" class="thumbnail"
+										data-rel="lightcase:myCollection:slideshow"
+										title="Your title">
+										<img src="img/photos/thumbs/dont-look-at-me-tn.jpg"/>
+									</a>
+								</div>
+								<div class="col-xs-3 col-md-3">
+									<a href="img/photos/flame-up-min.jpg" class="thumbnail"
+										data-rel="lightcase:myCollection:slideshow"
+										title="Your title">
+										<img src="img/photos/thumbs/flame-up-tn.jpg"/>
+									</a>
+								</div>
+								<div class="col-xs-3 col-md-3">
+									<a href="img/photos/grand-canyon-sunrise-1-min.jpg"
+										class="thumbnail" data-rel="lightcase:myCollection:slideshow" title="Your title">
+										<img src="img/photos/thumbs/grand-canyon-sunrise-1-tn.jpg"/>
+									</a>
+								</div>
+								<div class="col-xs-3 col-md-3">
+									<a href="img/photos/grand-canyon-sunrise-2-min.jpg"
+										class="thumbnail" data-rel="lightcase:myCollection:slideshow" title="Your title">
+										<img src="img/photos/thumbs/grand-canyon-sunrise-2-tn.jpg"/>
+									</a>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-xs-3 col-md-3">
+									<a href="img/photos/grand-canyon-sunrise-3-min.jpg"
+										class="thumbnail" data-rel="lightcase:myCollection:slideshow" title="Your title">
+										<img src="img/photos/thumbs/grand-canyon-sunrise-3-tn.jpg"/>
+									</a>
+								</div>
+								<div class="col-xs-3 col-md-3">
+									<a href="img/photos/grand-canyon-sunrise-4-min.jpg"
+										class="thumbnail" data-rel="lightcase:myCollection:slideshow" title="Your title">
+										<img src="img/photos/thumbs/grand-canyon-sunrise-4-tn.jpg"/>
+									</a>
+								</div>
+								<div class="col-xs-3 col-md-3">
+									<a href="img/photos/grand-canyon-sunrise-5-min.jpg"
+										class="thumbnail" data-rel="lightcase:myCollection:slideshow" title="Your title">
+										<img src="img/photos/thumbs/grand-canyon-sunrise-5-tn.jpg"/>
+									</a>
+								</div>
+								<div class="col-xs-3 col-md-3">
+									<a href="img/photos/grand-canyon-sunrise-6-min.jpg"
+										class="thumbnail" data-rel="lightcase:myCollection:slideshow"
+										title="Grand Canyon Sunrise 6 ">
+										<img src="img/photos/thumbs/grand-canyon-sunrise-6-tn.jpg"/>
+									</a>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-xs-3 col-md-3">
+									<a href="img/photos/hummingbird-fight-min.jpg" class="thumbnail"
+										data-rel="lightcase:myCollection:slideshow"
+										title="Your title">
+										<img src="img/photos/thumbs/hummingbird-fight-tn.jpg"/>
+									</a>
+								</div>
+								<div class="col-xs-3 col-md-3">
+									<a href="img/photos/half-rainbow-min.jpg" class="thumbnail"
+										data-rel="lightcase:myCollection:slideshow"
+										title="Your title">
+										<img src="img/photos/thumbs/half-rainbow-tn.jpg"/>
+									</a>
+								</div>
+								<div class="col-xs-3 col-md-3">
+									<a href="img/photos/moonlight-ojo-min.jpg" class="thumbnail"
+										data-rel="lightcase:myCollection:slideshow"
+										title="Your title">
+										<img src="img/photos/thumbs/moonlight-ojo-tn.jpg"/>
+									</a>
+								</div>
+								<div class="col-xs-3 col-md-3">
+									<a href="img/photos/slot-canyon-min.jpg" class="thumbnail"
+										data-rel="lightcase:myCollection:slideshow"
+										title="Your title">
+										<img src="img/photos/thumbs/slot-canyon-tn.jpg"/>
+									</a>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
-			</main>
+				<div class="row" id="contact">
+
+				</div>
+		</div>
+		</main>
 		</div>
 		<!-- begin footer-->
 		<footer>
 			<div class="container">
-				<div class="row">
+				<div class="row text-center">
 					<div class="col-xs-4 col-md-4">
 						<a href="https://twitter.com/gbloomDev"><i class="fa fa-twitter fa-3x"></i></a>
 					</div>
