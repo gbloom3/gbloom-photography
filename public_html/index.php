1d139cb260601c04a50e152d84d57c6253484b0a
Greg Bloom <gbloom3@cnm.edu>
2016-12-07 01:51:29 -0700

    moved form validator and mailer

--- public_html/index.php
+++ public_html/index.php
@@ -36,7 +36,7 @@
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
 
 		<!-- Your JavaScript Form Validator -->
-		<script src="js/form-validate.js"></script>
+		<script src="js/validate.js"></script>
 
 		<!-- Google reCAPTCHA -->
 		<script src='https://www.google.com/recaptcha/api.js'></script>
